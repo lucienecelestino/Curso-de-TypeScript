5909d4f1fb0bf39d180c2a06026b47c2e4515bf4
lucienecelestino <lucienecelestinodasilva@gmail.com.github>
2026-01-05 20:50:13 -0300

    Finalização  do projeto de Operações

--- projeto/build/index.js
+++ projeto/build/index.js
@@ -1,21 +1,71 @@
 "use strict";
-//console.log("TESTANDO SCRIPT")
 var input1 = document.getElementById('num1');
 var input2 = document.getElementById('num2');
 var buttonSoma = document.getElementById("buttonSomar");
 var buttonSubtrai = document.getElementById("buttonSubtrair");
+var buttonMultiplica = document.getElementById("buttonMultiplicar");
+var buttonDivide = document.getElementById("buttonDividir");
+var buttonPotencia = document.getElementById("buttonPotencia");
+var buttonTriangulo = document.getElementById("buttonTriangulo");
 var P_resultado = document.getElementById("resultado");
-function Somar(numero1, numero2) {
-    return numero1 + numero2;
+//uma forma de utilizar a interface
+function Somar(valor) {
+    return valor.numero1 + valor.numero2;
 }
 buttonSoma.addEventListener('click', function () {
-    var resultado = Somar(Number(input1.value), Number(input2.value)); //trasformando o textpo que vem do campo input para numero
+    var resultado = Somar({ numero1: Number(input1.value), numero2: Number(input2.value) }); //trasformando o textpo que vem do campo input para numero
     P_resultado.textContent = resultado.toString();
 });
-function Subtrair(numero1, numero2) {
+//outra forma de utilizar a interface
+function Subtrair(_a) {
+    var numero1 = _a.numero1, numero2 = _a.numero2;
     return numero1 - numero2;
 }
 buttonSubtrai.addEventListener('click', function () {
-    var resultado = Subtrair(Number(input1.value), Number(input2.value));
-    console.log(resultado);
+    var resultado = Subtrair({ numero1: Number(input1.value), numero2: Number(input2.value) });
+    P_resultado.textContent = resultado.toString();
+});
+//Sem utilizar interface
+function Multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+buttonMultiplica.addEventListener('click', function () {
+    var resultado = Multiplicar(Number(input1.value), Number(input2.value));
+    P_resultado.textContent = resultado.toString();
+});
+function operacao(_a) {
+    var tipo = _a.tipo, numero1 = _a.numero1, numero2 = _a.numero2;
+    if (tipo === "DIVIDIR") {
+        return numero1 / numero2;
+    }
+    else if (tipo === "POTENCIA") {
+        return Math.pow(numero1, numero2);
+    }
+    else {
+        return (numero1 * numero2) / 2;
+    }
+}
+buttonDivide.addEventListener('click', function () {
+    var resultado = operacao({
+        tipo: "DIVIDIR",
+        numero1: Number(input1.value),
+        numero2: Number(input2.value)
+    });
+    P_resultado.textContent = resultado.toString();
+});
+buttonPotencia.addEventListener('click', function () {
+    var resultado = operacao({
+        tipo: "POTENCIA",
+        numero1: Number(input1.value),
+        numero2: Number(input2.value)
+    });
+    P_resultado.textContent = resultado.toString();
+});
+buttonTriangulo.addEventListener('click', function () {
+    var resultado = operacao({
+        tipo: "AREA DO TRIANGULO",
+        numero1: Number(input1.value),
+        numero2: Number(input2.value)
+    });
+    P_resultado.textContent = resultado.toString();
 });
--- projeto/src/index.ts
+++ projeto/src/index.ts
@@ -1,28 +1,94 @@
-//console.log("TESTANDO SCRIPT")
+
 
 const input1 = document.getElementById('num1') as HTMLInputElement;
 const input2 = document.getElementById('num2') as HTMLInputElement;
 
+
 const buttonSoma = document.getElementById("buttonSomar") as HTMLInputElement;
 const buttonSubtrai = document.getElementById("buttonSubtrair") as HTMLInputElement;
+const buttonMultiplica = document.getElementById("buttonMultiplicar") as HTMLInputElement;
+const buttonDivide = document.getElementById("buttonDividir") as HTMLInputElement;
+const buttonPotencia = document.getElementById("buttonPotencia") as HTMLInputElement;
+const buttonTriangulo = document.getElementById("buttonTriangulo") as HTMLInputElement;
 
 const P_resultado = document.getElementById("resultado") as HTMLInputElement;
 
-function Somar(numero1: number,  numero2: number){
-    return numero1 + numero2;
+
+interface Valores{
+    numero1: number;
+    numero2: number
+}
+
+//uma forma de utilizar a interface
+function Somar(valor: Valores): number{
+    return valor.numero1 + valor.numero2;
 }
 
 buttonSoma.addEventListener('click', function(){
-    const resultado = Somar(Number(input1.value), Number(input2.value))//trasformando o textpo que vem do campo input para numero
+    const resultado = Somar({numero1: Number(input1.value), numero2: Number(input2.value)})//trasformando o textpo que vem do campo input para numero
     P_resultado.textContent = resultado.toString();
 
 })
-
-function Subtrair(numero1: number, numero2:number){
+//outra forma de utilizar a interface
+function Subtrair({numero1, numero2}: Valores): number{
     return numero1 - numero2;
 }
 
 buttonSubtrai.addEventListener('click', function(){
-    const resultado = Subtrair(Number(input1.value), Number(input2.value))
-    console.log(resultado);
+    const resultado = Subtrair({numero1: Number(input1.value), numero2: Number(input2.value)})
+    P_resultado.textContent = resultado.toString();
+})
+
+//Sem utilizar interface
+function Multiplicar(numero1: number, numero2:number): number{
+    return numero1 * numero2;
+}
+
+buttonMultiplica.addEventListener('click', function(){
+    const resultado = Multiplicar(Number(input1.value), Number(input2.value))
+    P_resultado.textContent = resultado.toString();
+})
+
+
+//USANDO TYPE ALIAS - não sendo mais necessario criar uma funcao para cada operacao
+type Operacoes = "DIVIDIR" | "POTENCIA" | "AREA DO TRIANGULO";
+interface Valores2{
+    numero1: number;
+    numero2: number;
+    tipo: Operacoes;
+}
+function operacao({tipo, numero1, numero2}: Valores2):number{
+    if(tipo === "DIVIDIR"){
+        return numero1 / numero2;
+    }else if(tipo === "POTENCIA"){
+        return numero1 ** numero2;
+    }else{
+        return (numero1*numero2)/2
+    }
+
+}
+
+buttonDivide.addEventListener('click', function(){
+    const resultado = operacao({
+        tipo: "DIVIDIR",
+        numero1: Number(input1.value),
+        numero2: Number(input2.value)})
+    P_resultado.textContent = resultado.toString();
+})
+
+
+buttonPotencia.addEventListener('click', function(){
+    const resultado = operacao({
+        tipo: "POTENCIA",
+        numero1: Number(input1.value),
+        numero2: Number(input2.value)})
+    P_resultado.textContent = resultado.toString();
+})
+
+buttonTriangulo.addEventListener('click', function(){
+    const resultado = operacao({
+        tipo: "AREA DO TRIANGULO",
+        numero1: Number(input1.value),
+        numero2: Number(input2.value)})
+    P_resultado.textContent = resultado.toString();
 })
